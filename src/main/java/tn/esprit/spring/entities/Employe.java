7d694ff82c230bf238b77e5315b30565387e96ae
Houcem Mahmoud <houcem.mahmoud@esprit.tn>
2022-01-27 23:46:58 +0100

    implemented-tests

--- src/main/java/tn/esprit/spring/entities/Employe.java
+++ src/main/java/tn/esprit/spring/entities/Employe.java
@@ -1,5 +1,105 @@
 package tn.esprit.spring.entities;
 
-public class Employe {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.OneToOne;
+
+@Entity
+public class Employe implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String prenom;
+
+	private String nom;
+
+	private String email;
+
+	private String password;
+
+	private boolean actif;
+
+	@OneToOne
+	private Contrat contrat;
+
+	public Employe() {
+		super();
+	}
+
+	public Employe(Long id, String prenom, String nom, String email, String password, boolean actif) {
+		super();
+		this.id = id;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.email = email;
+		this.password = password;
+		this.actif = actif;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isActif() {
+		return actif;
+	}
+
+	public void setActif(boolean actif) {
+		this.actif = actif;
+	}
+
+	public Contrat getContrat() {
+		return contrat;
+	}
+
+	public void setContrat(Contrat contrat) {
+		this.contrat = contrat;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 }
