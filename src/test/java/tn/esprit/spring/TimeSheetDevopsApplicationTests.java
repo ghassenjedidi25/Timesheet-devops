5b3a1b13ec0db4772eb431febac4e73fbb93d5da
Jedidi Ghassen <jedidighassen@gmail.com>
2022-01-28 17:35:11 +0100

    chore: added tests

--- src/test/java/tn/esprit/spring/TimeSheetDevopsApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimeSheetDevopsApplicationTests.java
@@ -1,9 +1,13 @@
 package tn.esprit.spring;
 
+import java.text.ParseException;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,18 +15,46 @@ import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.services.EmployeServiceImpl;
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 public class TimeSheetDevopsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 	@Autowired
 	EmployeServiceImpl es;
 
 	@Test
-	public void testRetrieveAllDepartements() {
+	@Order(1)
+	public void testRetrieveAllEmployees() {
 		List<Employe> listEmployees = es.retrieveAllEmployees();
 		Assertions.assertEquals(0, listEmployees.size());
+	}
+	@Test
+	@Order(2)
+	public void testAddEmployee() throws ParseException{
+		Employe e = new Employe("ghassen","jedidi", "jedidighassen@gmail.com", false, null);
+		Employe addedEmploye = es.addEmploye(e);
+		Assertions.assertEquals(e.getNom(),addedEmploye.getNom());
+	}
+	
+	@Test
+	@Order(3)
+	public void testUpdateEmployee() throws ParseException{
+		Employe e = new Employe("test","jedidi", "jedidighassen@gmail.com", false, null);
+		Employe updatedEmploye = es.updateEmploye(e);
+		Assertions.assertEquals(e.getNom(),updatedEmploye.getNom());
+	}
+	
+	@Test
+	@Order(4)
+	public void testRetriveEmployee() throws ParseException{
+		Employe userRetrived = es.retrieveEmploye("1");
+		Assertions.assertEquals(1L,userRetrived.getId());
+	}
+	
+	@Test
+	@Order(5)
+	public void testDeleteEmployee() throws ParseException{
+		es.deleteEmploye("1");
+		Assertions.assertNull(es.retrieveEmploye("1"));
 	}
 }
